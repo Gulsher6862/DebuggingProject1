8db35f576165105c457d442e47836c2a7bbb20e1
spandana <spandysama3@gmail.com>
2020-08-04 17:31:18 -0400

    Added BrandActivity

--- Project1/app/src/main/java/com/example/project1/Home.java
+++ Project1/app/src/main/java/com/example/project1/Home.java
@@ -2,13 +2,31 @@ package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+    ImageButton imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        imageView = (ImageButton) findViewById(R.id.imageView7);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, BrandActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+
+
 }
