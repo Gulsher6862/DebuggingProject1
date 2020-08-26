64e5bb81fe22336822c8b15eafaddfdac6c22873
Ashwini <ashwini.chaitankar28@gmail.com>
2020-08-26 16:01:12 +0530

    signup functionality added

--- Project1/app/src/main/java/com/example/project1/Signup.java
+++ Project1/app/src/main/java/com/example/project1/Signup.java
@@ -1,7 +1,9 @@
 package com.example.project1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,13 +12,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class Signup extends AppCompatActivity {
 
     EditText musername,mpassword,mphoneno,memail,mgender;
     Button msignup;
-
     TextView mhaveaccount;
+    private ProgressDialog mProgressDialog;
 
 
     @Override
@@ -31,12 +45,13 @@ public class Signup extends AppCompatActivity {
          memail = (EditText)findViewById(R.id.input_email);
         mhaveaccount = (TextView)findViewById(R.id.alreadaccount);
         msignup = (Button)findViewById(R.id.btnSignUp);
+        mProgressDialog = new ProgressDialog(this);
 
 
         msignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 String username = musername.getText().toString().trim();
+                String username = musername.getText().toString().trim();
                 String password = mpassword.getText().toString().trim();
                 String Email = memail.getText().toString().trim();
                  String phoneno = mphoneno.getText().toString().trim();
@@ -47,22 +62,23 @@ public class Signup extends AppCompatActivity {
                     return;
                 };
                 if (TextUtils.isEmpty(password)){
-                    mpassword.setError("Required Password");
+                    mpassword.setError("required password");
                     return;
                 }
                 if(TextUtils.isEmpty(Email)){
-                    memail.setError("Required Email");
-                    return;
-                }
-                if(TextUtils.isEmpty(phoneno)){
-                    mphoneno.setError("Required Phone no");
+                    memail.setError("required email");
                     return;
                 }
                 if(TextUtils.isEmpty(Gender)){
-                    mgender.setError("Required Gender");
+                    mgender.setError("required gender");
                     return;
                 }
-                startActivity(new Intent(getApplicationContext(),Home.class));
+                if(TextUtils.isEmpty(phoneno)){
+                    mphoneno.setError("required phone no");
+                    return;
+                }
+
+                createAccount(username,phoneno,password);
 
             }
         });
@@ -80,5 +96,57 @@ public class Signup extends AppCompatActivity {
         startActivity(intent);
     }
 
-   
+    private void createAccount(String name,String phone,String password){
+      mProgressDialog.setTitle("Create Account");
+      mProgressDialog.setMessage("checking the credentials...");
+      mProgressDialog.setCanceledOnTouchOutside(false);
+      mProgressDialog.show();
+
+      ValidatePhoneNo(name,phone,password);
+    }
+
+    private void ValidatePhoneNo(final String name, final String phone, final String password) {
+
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(!snapshot.child("Users").child(phone).exists()){
+                    HashMap<String,Object> userMap = new HashMap<>();
+                    userMap.put("phone",phone);
+                    userMap.put("password",password);
+                    userMap.put("name",name);
+                    RootRef.child("Users").child(phone).updateChildren(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                mProgressDialog.dismiss();
+                                Toast.makeText(getApplicationContext(),"Account created successfully",Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(getApplicationContext(),Login.class);
+                                startActivity(intent);
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mProgressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }else{
+                    Toast.makeText(getApplicationContext(),"this number already exists...",Toast.LENGTH_LONG).show();
+                    mProgressDialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
 }
