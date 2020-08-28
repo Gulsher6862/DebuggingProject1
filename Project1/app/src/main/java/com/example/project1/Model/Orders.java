326419032a93a5668eca9c24228958e427cdd89d
Spandana <spandysama3@gmail.com>
2020-08-28 17:27:27 +0530

    fetching order details from firestore

---
+++ Project1/app/src/main/java/com/example/project1/Model/Orders.java
@@ -0,0 +1,83 @@
+package com.example.project1.Model;
+
+public class Orders {
+    private String name,phone,address,city,totalAmount,orderId,date,time;
+
+    public Orders() {
+    }
+
+    public Orders(String name, String phone, String address, String city, String totalAmount, String orderId, String date, String time) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.totalAmount = totalAmount;
+        this.orderId = orderId;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
--- Project1/app/src/main/java/com/example/project1/MyOrders.java
+++ Project1/app/src/main/java/com/example/project1/MyOrders.java
@@ -1,14 +1,91 @@
 package com.example.project1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.project1.Model.Orders;
+import com.example.project1.Prevelant.Prevelant;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MyOrders extends AppCompatActivity {
+
+    private Button shopMore;
+    private RecyclerView mRecyclerView;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference orderRef = db.collection("Orders");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_orders);
+
+        shopMore = findViewById(R.id.shop_more_btn);
+        mRecyclerView = findViewById(R.id.orders_list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        shopMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Home.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirestoreRecyclerOptions<Orders> options = new FirestoreRecyclerOptions.Builder<Orders>()
+                .setQuery(orderRef.document(Prevelant.currentUser.getPhone()).collection("My Orders"),Orders.class)
+                .build();
+
+        FirestoreRecyclerAdapter<Orders,OrdersViewHolder> adapter = new FirestoreRecyclerAdapter<Orders, OrdersViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull OrdersViewHolder holder, int position, @NonNull Orders model) {
+                holder.userName.setText("Name : " + model.getName());
+                holder.userPhone.setText("Phone : " + model.getPhone());
+                holder.userAddress.setText("Address : " + model.getAddress() + ", " + model.getCity());
+                holder.userTotalPrice.setText("Total Price : " + model.getTotalAmount() + "$");
+                holder.userDate.setText("Order Date : " + model.getDate());
+                holder.userTime.setText("Order Time : " + model.getTime());
+            }
+
+            @NonNull
+            @Override
+            public OrdersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.orders_layout,parent,false);
+                return new OrdersViewHolder(view);
+            }
+        };
+        mRecyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class OrdersViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView userName,userPhone,userTotalPrice,userAddress,userDate,userTime;
+
+        public OrdersViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.order_name);
+            userPhone = itemView.findViewById(R.id.order_phone);
+            userTotalPrice = itemView.findViewById(R.id.order_price);
+            userAddress = itemView.findViewById(R.id.order_address);
+            userDate = itemView.findViewById(R.id.order_date);
+            userTime = itemView.findViewById(R.id.order_time);
+        }
     }
 }
